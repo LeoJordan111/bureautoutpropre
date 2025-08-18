936c9063f56101516aa74a702874cdd85fa1e1a4
Traur111 <leojordan.111@gmail.com>
2025-08-18 16:45:30 +0200

    Mise en place du git

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// src="./assets/js/script.js"
+
+console.log('test')
+const btn = document.querySelector("#go");
+
+btn.addEventListener("onClick", (e) => {
+    console.log('testbouton')
+})
+    
+
